40f05d8d58bfcd24dae880086b19983182622f83
awaiting-fallback <hdubeux@proton.me>
2025-05-05 21:37:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 window.onload = () => {
   setTimeout(() => {
     document.getElementById("acessar").style.opacity = 1;
-  }, 4000); // botão aparece depois de 4 segundos
+  }, 4000);
 
   document.getElementById("acessar").addEventListener("click", () => {
+    // Esconde a primeira camada (frase + botão)
+    document.querySelector(".glitch").style.display = "none";
+    document.getElementById("acessar").style.display = "none";
+
+    // Mostra a camada 2 com fallback
     document.getElementById("camada2").classList.remove("oculto");
     document.getElementById("camada2").classList.add("visivel");
   });
